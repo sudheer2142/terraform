775e76f0d15c88b9c17236ce31bb528013f338cc
Sudheer <info.sudheer30@gmail.com>
2023-09-27 17:01:31 +0530

    updated the code of terraform with project automation

---
+++ roboshop-infra-user/outputs.tf
@@ -0,0 +1,23 @@
+# output "vpc_id" {
+#   value = module.vpc.vpc_id # module.<name>.<output-variable-name>
+#  }
+
+output "public_subnet_ids" {
+  value = local.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = local.private_subnet_ids
+}
+
+output "database_subnet_ids" {
+  value = local.database_subnet_ids
+}
+
+# output "ami_id" {
+#   value = data.aws_ami.devops_ami.id
+# }
+
+# output "ips" {
+#   value = local.ips
+# }
